57ccec433a63e3ca1ddf50120f749514eec8b98a
anivkuttan <anivkutta196@gmail.com>
2023-08-24 00:05:37 +0530

    theme switch added

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_textfieldflutter_textfield/src/sample_1.dart';
+import 'package:flutter_textfieldflutter_textfield/src/TextFields/sample_1.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+ValueNotifier<bool> c = ValueNotifier(false);
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ValueListenableBuilder(
+      valueListenable: c,
+      builder: (context, value, child) => MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: value ? Brightness.light : Brightness.dark,
+          colorScheme: ColorScheme.fromSeed(
+            brightness: value ? Brightness.light : Brightness.dark,
+            seedColor: Colors.deepPurple,
+          ),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -37,6 +52,13 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Text Fields'),
+        actions: [
+          Switch(
+              value: c.value,
+              onChanged: (value) {
+                c.value = !c.value;
+              })
+        ],
       ),
       body: Center(
         child: Column(
